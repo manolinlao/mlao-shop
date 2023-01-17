8054a544636b8b9cc90ad4d88e009908d049f0a1
mlao <manolinlao@gmail.com>
2023-01-17 13:08:21 +0100

    desarrollo

--- src/app/core/components/form/form.component.ts
+++ src/app/core/components/form/form.component.ts
@@ -8,7 +8,7 @@ import {
 import { MessageService } from '../../../messages/message.service';
 import { Message } from 'src/app/messages/message.model';
 import { Product } from '../../../model/product.model';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -17,6 +17,22 @@ import { NgForm } from '@angular/forms';
 export class FormComponent {
   product: Product = new Product();
   editing: boolean = false;
+
+  // Para el formulario reactivo
+  nameField: FormControl = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern('^[A-Za-z ]+$'),
+    ],
+    updateOn: 'change',
+  });
+  categoryField: FormControl = new FormControl();
+
+  productForm: FormGroup = new FormGroup({
+    name: this.nameField,
+    category: this.categoryField,
+  });
 
   constructor(
     private model: Model,
@@ -27,24 +43,59 @@ export class FormComponent {
     this.messageService.reportMessage(new Message('Creating new product'));
   }
 
+  ngOnInit() {
+    // escucha el statusChanges del productForm que es un FormGroup
+    this.productForm.statusChanges.subscribe((newStatus) => {
+      if (newStatus == 'INVALID') {
+        let invalidControls: string[] = [];
+        for (let controlName in this.productForm.controls) {
+          if (this.productForm.controls[controlName].invalid) {
+            invalidControls.push(controlName);
+          }
+        }
+        this.messageService.reportMessage(
+          new Message(`INVALID: ${invalidControls.join(',')}`)
+        );
+      } else {
+        this.messageService.reportMessage(new Message(newStatus));
+      }
+    });
+
+    // escucha al statusChanges del nameField
+    /*
+    this.nameField.statusChanges.subscribe((newStatus) => {
+      if (newStatus == 'INVALID' && this.nameField.errors != null) {
+        let errs = Object.keys(this.nameField.errors).join(', ');
+        this.messageService.reportMessage(new Message(`INVALID: ${errs} `));
+      } else {
+        this.messageService.reportMessage(new Message(newStatus));
+      }
+    });
+    */
+    /*
+    // escucha los cambios del nameField
+    this.nameField.valueChanges.subscribe((newValue) => {
+      this.messageService.reportMessage(new Message(newValue || '(Empty)'));
+      if (typeof newValue == 'string' && newValue.length % 2 == 0) {
+        this.nameField.markAsPristine();
+      }
+    });
+    */
+  }
+
   handleStateChange(newState: StateUpdate) {
     this.editing = newState.mode == MODES.EDIT;
     if (this.editing && newState.id) {
-      this.product = { ...this.model.getProduct(newState.id) };
+      this.product = { ...this.model.getProduct(newState.id) } ?? new Product();
       this.messageService.reportMessage(
         new Message(`editing ${this.product.name}`)
       );
+      //this.nameField.setValue(this.product.name);
     } else {
       this.product = new Product();
       this.messageService.reportMessage(new Message('creating new product'));
+      //this.nameField.setValue('');
     }
-  }
-
-  submitForm(form: NgForm) {
-    if (form.valid) {
-      this.model.saveProduct(this.product);
-      this.product = new Product();
-      form.resetForm();
-    }
+    this.productForm.reset(this.product);
   }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './components/table/table.component';
 import { FormComponent } from './components/form/form.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedStateService } from './services/sharedState.service';
 import { ModelModule } from '../model/model.module';
+import { ValidationHelperPipe } from './pipes/validation-helper.pipe';
 
 @NgModule({
-  declarations: [TableComponent, FormComponent],
-  imports: [CommonModule, FormsModule, ModelModule],
+  declarations: [TableComponent, FormComponent, ValidationHelperPipe],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ModelModule],
   exports: [TableComponent, FormComponent],
   providers: [SharedStateService],
 })
